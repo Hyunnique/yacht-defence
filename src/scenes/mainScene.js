01a85ad2bac31b2da65d535571d1bf58992ef7fb
koojawon <jawonkoo@naver.com>
2022-09-24 17:23:20 +0900

    modified:   src/objects/units/playerUnit.js 	modified:   src/objects/units/test.js 	modified:   src/scenes/gameScene.js 	modified:   src/scenes/mainScene.js

--- src/scenes/mainScene.js
+++ src/scenes/mainScene.js
@@ -8,17 +8,6 @@ export default class MainScene extends Phaser.Scene{
 
     create() {
 
-        var bg = this.add.graphics();
-        bg.fillStyle(0x000000,1);
-        bg.fillRect(32, 32, 300, 200);
-        //bg.setScrollFactor(0);
-        bg.setDepth(50);
-        console.log("success?");
-
-        this.add.bitmapText(Config.width / 2, 150, "pixelFont", "MainScene", 20).setOrigin(0.5);
-        
-        this.add.sprite(Config.width / 2, Config.height / 2, "testwait");
-        
         this.add.text(Config.width / 2, Config.height / 2, "Press W", {
             fontSize: `20px`,
             color: 0xFFFFFF
