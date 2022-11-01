c34e31d39846b2ccd114b3cd12e2f7850da55ea4
koojawon <jawonkoo@naver.com>
2022-11-01 21:22:20 +0900

    modified:   src/Config.js         플러그인 사용 	modified:   src/objects/mobs/Mob.js         경로 따라 이동하게 	new file:   src/objects/points/mobPath.js         경로 저장용 	modified:   src/scenes/gameScene.js         ??

---
+++ src/objects/points/mobPath.js
@@ -0,0 +1,9 @@
+const pathA = new Phaser.Curves.Path(0, 312).lineTo(648, 312).lineTo(648, 1128).lineTo(1752, 1128).lineTo(1752, 720).lineTo(2400, 720);
+const pathB = new Phaser.Curves.Path(0, 1128).lineTo(648, 1128).lineTo(648, 312).lineTo(1752, 312).lineTo(1752, 720).lineTo(2400, 720);
+const pathBoss = new Phaser.Curves.Path(0, 312).lineTo(2400, 720);
+
+export {
+    pathA,
+    pathB,
+    pathBoss
+}
