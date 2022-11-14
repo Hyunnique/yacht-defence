e44e2fbc988ad3dec50ff556cf48ebef79a8c276
Seol-Dev <seol3964@naver.com>
2022-11-14 23:40:11 +0900

    Dice Phase 기능 구현

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import GameSceneCss from "../views/gameScene.css";
 import SilverFont_ttf from "./assets/font/Silver.ttf";
 import SilverFont_woff from "./assets/font/Silver.woff";
 
+import Dice1 from "./assets/images/dice_1.png";
+import Dice2 from "./assets/images/dice_2.png";
+import Dice3 from "./assets/images/dice_3.png";
+import Dice4 from "./assets/images/dice_4.png";
+import Dice5 from "./assets/images/dice_5.png";
+
 import DiceAnimation1 from "./assets/images/dice_animated_1.gif";
 import DiceAnimation2 from "./assets/images/dice_animated_2.gif";
 import DiceAnimation3 from "./assets/images/dice_animated_3.gif";
