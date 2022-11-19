642f8e4ac50e5e4ee76582283e5e8ab8ad80c5ef
koojawon <jawonkoo@naver.com>
2022-11-19 17:02:13 +0900

    modified:   autoGen.js 	modified:   src/assets/specsheets/roundSheet.json 	modified:   src/objects/units/playerUnit.js 	modified:   src/scenes/gameScene.js
    라운드 json변경

--- autoGen.js
+++ autoGen.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 
-var dictObject = [];
+var dictObject = {};
 for (var i = 0; i < 50; i++) {
+    var dataArray = [];
     var datadict = {};
 
     datadict["mobName"] = "BatSmallA";
     datadict["mobCount"] = 10;
     datadict["hpFactor"] = 1.0;
     datadict["mobRoute"] = "A";
-
-    dictObject.push(datadict);
+    dataArray.push(datadict);
+    dictObject["round"+i] = dataArray;
 }
 
 fs.writeFile("../roundSheet.json",JSON.stringify(dictObject),()=>{});
