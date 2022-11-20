77e9c605161cd0dad55ef68ef7347aa7aa9086b5
jsh031613 <jsh031613@naver.com>
2022-11-20 20:33:11 +0900

    unit effect 재생 일시 제거

--- src/objects/units/unitEffect.js
+++ src/objects/units/unitEffect.js
@@ -4,9 +4,10 @@ const Phaser = require("phaser");
 export default class UnitEffect extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, effectName) {
         super(scene, x, y, effectName);
+        this.effectName = effectName
     }
 
     playEffect() {
-        this.play(effectName);
+        this.play(this.effectName, true);
     }
 }
