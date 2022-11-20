59975b6bb52e766641d039d0a00e4068271c4f92
koojawon <jawonkoo@naver.com>
2022-11-20 21:59:36 +0900

    modified:   src/objects/units/playerUnit.js 	modified:   src/objects/units/unitEffect.js 	modified:   src/scenes/preloadScene.js         이펙트 초기 출력

--- src/objects/units/unitEffect.js
+++ src/objects/units/unitEffect.js
@@ -4,5 +4,15 @@ const Phaser = require("phaser");
 export default class UnitEffect extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, attacker) {
         super(scene, attacker.x, attacker.y, attacker.effectName);
+        this.attacker = attacker;
+        scene.add.existing(this);
+        this.alpha = 0;
+    }
+
+    playEffect()
+    {
+        console.log(this);
+        this.alpha = 1;
+        this.play(this.attacker.effectName);
     }
 }
