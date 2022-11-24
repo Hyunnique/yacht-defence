48739da979cde714f740e70e9132998ceaf74a0a
Seol-Dev <seol3964@naver.com>
2022-11-25 01:52:12 +0900

    서버 터지는 문제 땜빵

--- js/GameServer.js
+++ js/GameServer.js
@@ -258,10 +258,9 @@ module.exports = {
 
     onPlayerBaseDamage(socket, roomId) {
         socket.on('playerInfo-baseDamage', (msg) => {
-            console.log("Player " + (msg.index + 1) + " Hit!");
 
             if (!this.Rooms[roomId].players[msg.index]) return;
-            
+
             this.Rooms[roomId].players[msg.index].hp -= msg.damage;
             this.syncPlayerInfo(roomId);
         });
@@ -296,6 +295,9 @@ module.exports = {
 
     onChatMessage(socket, roomId) {
         socket.on('chat-message', (msg) => {
+
+            if (!this.Rooms[roomId].players[msg.playerIndex]) return;
+
             this.emitAll(roomId, 'chat-message', {
                 playerIndex: msg.playerIndex,
                 name: this.Rooms[roomId].players[msg.playerIndex].name,
