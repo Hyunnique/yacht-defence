783fe7fcfc5fe50e3b1eedf1e8e156bccf32271c
koojawon <jawonkoo@naver.com>
2022-11-26 01:45:30 +0900

    modified:   src/Game.js 	modified:   src/scenes/gameScene.js

--- src/Game.js
+++ src/Game.js
@@ -187,6 +187,7 @@ var Game = {
         });
 
         this.Socket.on("player-unitData", (msg) => {
+            this.GameObject.scene.getScene("gameScene").removeOtherPlayerUnit();
             this.GameObject.scene.getScene("gameScene").spectate_player = msg;
             this.GameObject.scene.getScene("gameScene").placeOtherPlayerUnit();
         });
