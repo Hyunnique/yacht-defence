53627076af6e9d1a12bc9126ba7665419d417314
koojawon <jawonkoo@naver.com>
2022-11-26 18:40:46 +0900

    modified:   src/objects/mobs/Mob.js 	modified:   src/objects/points/mobPath.js 	modified:   src/objects/projectiles/bomb.js 	modified:   src/objects/projectiles/homing.js 	modified:   src/objects/projectiles/penetrate.js 	modified:   src/objects/units/playerUnit.js 	modified:   src/scenes/gameScene.js         간이 멀티 구현 기초 작업

--- src/objects/points/mobPath.js
+++ src/objects/points/mobPath.js
@@ -1,16 +1,47 @@
 const Phaser = require("phaser");
-const pathA = new Phaser.Curves.Path(0, 312).lineTo(648, 312).lineTo(648, 1128).lineTo(1752, 1128).lineTo(1752, 720).lineTo(2400, 720);
-const pathB = new Phaser.Curves.Path(0, 1128).lineTo(648, 1128).lineTo(648, 312).lineTo(1752, 312).lineTo(1752, 720).lineTo(2400, 720);
-const pathC = new Phaser.Curves.Path(1224, 0).lineTo(1224, 312).lineTo(648, 312).lineTo(648, 1128).lineTo(1752, 1128).lineTo(1752, 720).lineTo(2400, 720);
-const pathD = new Phaser.Curves.Path(1224, 1392).lineTo(1224, 1128).lineTo(648, 1128).lineTo(648, 312).lineTo(1752, 312).lineTo(1752, 720).lineTo(2400, 720);
-// const pathC = new Phaser.Curves.Path(1224, 0).lineTo(1224, 312).lineTo(1752, 312).lineTo(1752, 720).lineTo(2400, 720);
-// const pathD = new Phaser.Curves.Path(1224, 1392).lineTo(1224, 1128).lineTo(1752, 1128).lineTo(1752, 720).lineTo(2400, 720);
-const pathBoss = new Phaser.Curves.Path(0, 720).lineTo(2400, 720);
+const pathA0 = new Phaser.Curves.Path(0, 312).lineTo(648, 312).lineTo(648, 1128).lineTo(1752, 1128).lineTo(1752, 720).lineTo(2400, 720);
+const pathB0 = new Phaser.Curves.Path(0, 1128).lineTo(648, 1128).lineTo(648, 312).lineTo(1752, 312).lineTo(1752, 720).lineTo(2400, 720);
+const pathC0 = new Phaser.Curves.Path(1224, 0).lineTo(1224, 312).lineTo(648, 312).lineTo(648, 1128).lineTo(1752, 1128).lineTo(1752, 720).lineTo(2400, 720);
+const pathD0 = new Phaser.Curves.Path(1224, 1392).lineTo(1224, 1128).lineTo(648, 1128).lineTo(648, 312).lineTo(1752, 312).lineTo(1752, 720).lineTo(2400, 720);
+const pathBoss0 = new Phaser.Curves.Path(0, 720).lineTo(2400, 720);
+
+const pathA1 = new Phaser.Curves.Path(2400, 312).lineTo(3048, 312).lineTo(3048, 1128).lineTo(4152, 1128).lineTo(4152, 720).lineTo(4800, 720);
+const pathB1 = new Phaser.Curves.Path(2400, 1128).lineTo(3048, 1128).lineTo(3048, 312).lineTo(4152, 312).lineTo(4152, 720).lineTo(4800, 720);
+const pathC1 = new Phaser.Curves.Path(3624, 0).lineTo(3624, 312).lineTo(3048, 312).lineTo(3048, 1128).lineTo(4152, 1128).lineTo(4152, 720).lineTo(4800, 720);
+const pathD1 = new Phaser.Curves.Path(3624, 1392).lineTo(3624, 1128).lineTo(3048, 1128).lineTo(3048, 312).lineTo(4152, 312).lineTo(4152, 720).lineTo(4800, 720);
+const pathBoss1 = new Phaser.Curves.Path(2400, 720).lineTo(4800, 720);
+
+const pathA2 = new Phaser.Curves.Path(0, 1752).lineTo(648, 1752).lineTo(648, 2568).lineTo(1752, 2568).lineTo(1752, 2160).lineTo(2400, 2160);
+const pathB2 = new Phaser.Curves.Path(0, 2568).lineTo(648, 2568).lineTo(648, 1752).lineTo(1752, 1752).lineTo(1752, 2160).lineTo(2400, 2160);
+const pathC2 = new Phaser.Curves.Path(1224, 1440).lineTo(1224, 1752).lineTo(648, 1752).lineTo(648, 2568).lineTo(1752, 2568).lineTo(1752, 2160).lineTo(2400, 2160);
+const pathD2 = new Phaser.Curves.Path(1224, 2832).lineTo(1224, 2568).lineTo(648, 2568).lineTo(648, 1752).lineTo(1752, 1752).lineTo(1752, 2160).lineTo(2400, 2160);
+const pathBoss2 = new Phaser.Curves.Path(0, 2160).lineTo(2400, 2160);
+
+const pathA3 = new Phaser.Curves.Path(2400, 1752).lineTo(3048, 1752).lineTo(3048, 2568).lineTo(4152, 2568).lineTo(4152, 2160).lineTo(4800, 2160);
+const pathB3 = new Phaser.Curves.Path(2400, 2568).lineTo(3048, 2568).lineTo(3048, 1752).lineTo(4152, 1752).lineTo(4152, 2160).lineTo(4800, 2160);
+const pathC3 = new Phaser.Curves.Path(3624, 1440).lineTo(3624, 1752).lineTo(3048, 1752).lineTo(3048, 2568).lineTo(4152, 2568).lineTo(4152, 2160).lineTo(4800, 2160);
+const pathD3 = new Phaser.Curves.Path(3624, 2832).lineTo(3624, 2568).lineTo(3048, 2568).lineTo(3048, 1752).lineTo(4152, 1752).lineTo(4152, 2160).lineTo(4800, 2160);
+const pathBoss3 = new Phaser.Curves.Path(2400, 2160).lineTo(4800, 2160);
 
 export {
-    pathA,
-    pathB,
-    pathC,
-    pathD,
-    pathBoss
+    pathA0,
+    pathB0,
+    pathC0,
+    pathD0,
+    pathBoss0,
+    pathA1,
+    pathB1,
+    pathC1,
+    pathD1,
+    pathBoss1,
+    pathA2,
+    pathB2,
+    pathC2,
+    pathD2,
+    pathBoss2,
+    pathA3,
+    pathB3,
+    pathC3,
+    pathD3,
+    pathBoss3
 }
