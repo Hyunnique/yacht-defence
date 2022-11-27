59d8dfa6aff06da54763c4c8d3f07f2f5b38af89
Seol-Dev <seol3964@naver.com>
2022-11-27 19:40:01 +0900

    플레이어 사망시 랭킹 등록

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router');
 const router = new Router();
 const fs = require('fs').promises;
+const db = require('../schemas');
 
 router.get("/external/socket.io.min.js", async (ctx, next) => {
     ctx.body = await fs.readFile("./node_modules/socket.io/client-dist/socket.io.min.js", 'utf8');
@@ -8,6 +9,12 @@ router.get("/external/socket.io.min.js", async (ctx, next) => {
 
 router.get("/external/socket.io.min.js.map", async (ctx, next) => {
     ctx.body = await fs.readFile("./node_modules/socket.io/client-dist/socket.io.min.js.map", 'utf8');
+});
+
+router.get("/ranking", async (ctx, next) => {
+    let rankingResult = await db.PlayData.find().sort({ rounds: -1 }).limit(10);
+
+    ctx.body = rankingResult;
 });
 
 module.exports = router;
