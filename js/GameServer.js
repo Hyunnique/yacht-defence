f8917bf50a01f1c4888faa213d1b6e0b392e5c0a
Seol-Dev <seol3964@naver.com>
2022-11-28 00:19:53 +0900

    난이도 계수 완화, 보스전 잡몹 코스트 완화, 랭킹 페이지 수정

--- js/GameServer.js
+++ js/GameServer.js
@@ -58,12 +58,12 @@ module.exports = {
         let waveResult = waveGenerator(this.Rooms[roomId].generatorInfo.sheet, this.Rooms[roomId].roundInfo.num, this.Rooms[roomId].generatorInfo.cost, this.Rooms[roomId].generatorInfo.hpFactor);
 
         if (this.Rooms[roomId].roundInfo.num % 10 == 0) {
-            this.Rooms[roomId].generatorInfo.cost = Math.floor(this.Rooms[roomId].generatorInfo.cost * 1.3 + (30 * Math.pow(1.06, this.Rooms[roomId].roundInfo.num)));
+            this.Rooms[roomId].generatorInfo.cost = Math.floor(this.Rooms[roomId].generatorInfo.cost * 1.3 + (25 * Math.pow(1.06, this.Rooms[roomId].roundInfo.num)));
         } else {
-            this.Rooms[roomId].generatorInfo.cost = Math.floor(this.Rooms[roomId].generatorInfo.cost * 1.06 + (30 * Math.pow(1.06, this.Rooms[roomId].roundInfo.num)));
+            this.Rooms[roomId].generatorInfo.cost = Math.floor(this.Rooms[roomId].generatorInfo.cost * 1.06 + (25 * Math.pow(1.06, this.Rooms[roomId].roundInfo.num)));
         }
 
-        this.Rooms[roomId].generatorInfo.hpFactor = (this.Rooms[roomId].generatorInfo.hpFactor * 1.055 + (0.05 * Math.pow(1.06, this.Rooms[roomId].roundInfo.num))).toFixed(2);
+        this.Rooms[roomId].generatorInfo.hpFactor = (this.Rooms[roomId].generatorInfo.hpFactor * 1.055 + (0.05 * Math.pow(1.05, this.Rooms[roomId].roundInfo.num))).toFixed(2);
 
         this.emitAll(roomId, 'game-wavedata', waveResult);
 
