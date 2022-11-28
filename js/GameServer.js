beb2fadda3bfbad9058d5d67cf3c68b780c067b7
koojawon <jawonkoo@naver.com>
2022-11-28 13:26:18 +0900

    modified:   js/GameServer.js

--- js/GameServer.js
+++ js/GameServer.js
@@ -329,7 +329,7 @@ module.exports = {
                 });
 
                 const choiceRewardByPlayers = [
-                    [100000],
+                    [10],
                     [15, 5],
                     [15, 10, 5],
                     [20, 15, 5, 0]
