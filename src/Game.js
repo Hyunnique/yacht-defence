96f615671f0a98438424e310729b7662e1a0a6d6
jsh031613 <jsh031613@naver.com>
2022-11-28 21:25:14 +0900

    게임 종료 후 메인 로비로 돌아왔을 때, 기존의 매칭 메세지가 남아있지 않게 바꿈

--- src/Game.js
+++ src/Game.js
@@ -773,6 +773,7 @@ var Game = {
             this.hideUI("diceScene-default");
             this.hideUI("diceScene-result");
             this.showUI("mainScene-default");
+            document.getElementsByClassName("mainScene-message")[0].innerText = "";
         })
     },
 
