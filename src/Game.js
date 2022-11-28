d5d919361ad437b91a610aa813e417462776c586
jsh031613 <jsh031613@naver.com>
2022-11-28 23:08:21 +0900

    로비로 되돌아가도 볼륨 조절창은 남아있게 변경

--- src/Game.js
+++ src/Game.js
@@ -767,7 +767,6 @@ var Game = {
         this.Socket.on('game-end', (msg) => {
             this.GameObject.scene.getScene("gameScene").scene.stop().start("mainScene");
             this.hideUI("gameScene-topFloating");
-            this.hideUI("gameScene-topRightFloating");
             this.hideUI("gameScene-midFloating");
             this.hideUI("gameScene-bottomFloating");
             this.hideUI("common-shop");
